e5829b197e17e3307ed2a09ea387360fa828987a
Jasper Vermeulen <jaspervermeulen@icloud.com>
2024-02-28 14:26:54 +0100

    update

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -21,20 +21,22 @@ export default async function Layout({
 
   return (
     <div className="max-w-7xl mx-auto">
-      <Header>
-        <div className="flex items-center gap-10">
-          <Link href="/calendar" className="text-lg font-semibold">
-            Chronobill {'->'}
-          </Link>
-          <div className="flex items-center gap-2">
-            <Link href="/calendar">Calendar</Link>
-            <Link href="/clients">Clients</Link>
-            <Link href="/clients">Invoices</Link>
+      <div className="mx-4">
+        <Header>
+          <div className="flex items-center gap-10">
+            <Link href="/calendar" className="text-lg font-semibold">
+              Chronobill {'->'}
+            </Link>
+            <div className="flex items-center gap-2">
+              <Link href="/calendar">Calendar</Link>
+              <Link href="/clients">Clients</Link>
+              <Link href="/clients">Invoices</Link>
+            </div>
           </div>
-        </div>
-        <AuthButton />
-      </Header>
-      <div className="py-10">{children}</div>
+          <AuthButton />
+        </Header>
+        <div className="py-10">{children}</div>
+      </div>
     </div>
   );
 }
