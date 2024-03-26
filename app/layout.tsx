2167402f3ecc863aaa06a74cfe44acbc20c8c09d
Jasper <jaspervermeulen@icloud.com>
2024-03-26 21:28:19 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import { GeistSans } from 'geist/font/sans';
 import './globals.css';
 
+export const metadata = {
+  title:
+    'Forward - we build visually and technically sound web experiences for humans',
+  description:
+    'Forward is a tiny spec on the internet, we build visually and technically sound web experiences for humans. We build applications that solve problems and make the web a better place.',
+};
+
 export default function RootLayout({
   children,
 }: {
