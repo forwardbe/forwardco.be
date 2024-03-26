3a6a0ca5539bacf633ef4a61e9d107efd9f43486
Jasper <jaspervermeulen@icloud.com>
2024-03-26 20:43:25 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,5 @@
 import { GeistSans } from 'geist/font/sans';
 import './globals.css';
-import { Toaster } from 'react-hot-toast';
-import Navigation from '@/components/Navigation';
-
-const defaultUrl = process.env.WEBSITE_URL
-  ? `https://${process.env.WEBSITE_URL}`
-  : 'http://localhost:3000';
-
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: 'Chronobill - Time tracking & invoicing',
-  description:
-    'Chronobill makes it easy to track time across all your projects. Then turn that data into reporting or invoices.',
-};
 
 export default function RootLayout({
   children,
@@ -22,13 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <body>
-        <Toaster />
-        <main>
-          <Navigation />
-          <div className="max-w-6xl mx-auto">
-            <div className="mx-4">{children}</div>
-          </div>
-        </main>
+        <main>{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,72 +1,26 @@
-import Image from 'next/image';
-import BuilderImage from '@/assets/images/builder.png';
-
-import Button from '@/components/Button';
-import ProductCard from '@/components/ProductCard';
-import { web } from '@/data/services';
-import { products } from '@/data/products';
+import { Logo } from '@/components/logo';
+import Link from 'next/link';
 
 export default function Index() {
   return (
-    <div>
-      <div className="py-24 border-b flex items-center justify-between">
-        <div>
-          <p className="text-5xl w-5/6">
-            I'm a digital designer and developer creating digital experiences
-          </p>
-          <p className="w-2/3 mt-4 mb-8">
-            I design and build software with the goal of telling stories,
-            reducing friction, and crafting digital experiences that are a joy
-            to use.
-          </p>
-          <Button as="link" href="/">
-            Get to know me
-          </Button>
-        </div>
-        <Image src={BuilderImage} alt="Builder" width={250} />
-      </div>
-      <div className="py-24 border-b flex">
-        <div>
-          <p className="text-3xl">What do I do</p>
-          <p className="w-2/3 mt-4 mb-8">
-            I design and build software with the goal of telling stories,
-            reducing friction, and crafting digital experiences that are a joy
-            to use.
-          </p>
-          <Button as="link" href="/">
-            See services
-          </Button>
-        </div>
-        <div>
-          <ul className="flex flex-col gap-2">
-            {web.map((item) => (
-              <li key={item} className="whitespace-nowrap">
-                — {item}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div className="py-24 border-b flex flex-col">
-        <div>
-          <p className="text-3xl">
-            I like to build products (read little side projects)
-          </p>
-          <p className="w-2/3 mt-4 mb-8">
-            I design and build software with the goal of telling stories,
-            reducing friction, and crafting digital experiences that are a joy
-            to use.
-          </p>
-          <Button as="link" href="/">
-            See all side projects
-          </Button>
-        </div>
-        <div className="grid md:grid-cols-2 mt-12 gap-x-6 gap-y-12">
-          {products.map((item) => (
-            <ProductCard key={item.id} {...item} />
-          ))}
-        </div>
-      </div>
+    <div className="max-w-xl mx-4 md:mx-10 py-10 md:py-20">
+      <Logo />
+      <p className="mt-8 text-neutral-600">
+        Forward is a tiny spec on the internet, we build visually and
+        technically sound web experiences for humans. We build applications that
+        solve problems and make the web a better place.
+      </p>
+      <p className="mt-4 mb-12 text-neutral-600">
+        Great things can be achieved through a combination of unwavering passion
+        and perseverance in the face of adversity. We are passionate and are
+        constantly learning and growing.
+      </p>
+      <Link
+        className="text-sm bg-black text-white px-4 py-3 rounded-full"
+        href="mailto:jasper@forwardco.be"
+      >
+        Contact
+      </Link>
     </div>
   );
 }
